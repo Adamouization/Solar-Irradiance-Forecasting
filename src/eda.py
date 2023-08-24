9b265d23605077f781f3d6cb92b97b9d3f01ff0d
ajaamour <adam@jaamour.com>
2023-08-24 09:13:23 +0000

    WIP: ARIMA implementation

--- src/eda.py
+++ src/eda.py
@@ -4,3 +4,10 @@ def plot_time_series(df, time_col, plot_cols):
     _ = plot_features.plot(subplots=True, rot=90, figsize=(15, len(plot_cols)))
 
 
+def print_df_index_date_range(df, sample_str: str):
+    print("{sample} dataset date ranges = {date_min} to {date_max}".format(
+        sample=sample_str,
+        date_min=min(df.index), 
+        date_max=max(df.index)
+    ))
+    print()
