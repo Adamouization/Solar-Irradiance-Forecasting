78e3897dec308ea3d334de5ccab2613ea238e34b
ajaamour <adam@jaamour.com>
2023-08-24 09:14:42 +0000

    Move env setup files to env dir

--- env/init.sh
+++ env/init.sh
@@ -8,9 +8,6 @@ echo "Upading apt-get and intalling the latest version of 'libgl1-mesa-glx'..."
 sudo apt-get update 
 sudo apt-get install -y libgl1-mesa-glx 
 
-echo "Installing Python libraries..."
-pip install scikit-learn tqdm
-
 echo "Exporting variables..."
 export TF_ENABLE_ONEDNN_OPTS=0
 
